4e81c90a60d3e0262c70133f77b972ea0f958524
danerybaez27 <104737615+danerybaez27@users.noreply.github.com>
2022-05-02 14:45:26 -0400

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -6,7 +6,7 @@ let handler = async(m, { conn, text }) => {
     var nomor = m.sender
     const teks1 = `*_❒═════[REPORTE]═════╾❒_*\n*_┬_*\n*_├Numero: wa.me/${nomor.split("@s.whatsapp.net")[0]}_*\n*_┴_*\n*_┬_*\n*_├Mensaje: ${text}_*\n*_┴_*`
     conn.sendMessage('15619717826@s.whatsapp.net', teks1, MessageType.text)
-    //conn.sendMessage('51933748331@s.whatsapp.net', teks1, MessageType.text) // añade tu número aqui, pero no quites el otro. Quita las // del inicio de esta línea
+    //conn.sendMessage('18292077732@s.whatsapp.net', teks1, MessageType.text) // añade tu número aqui, pero no quites el otro. Quita las // del inicio de esta línea
     conn.reply(m.chat, '*_✅ El problema se ha informado al propietario del Bot ✅_*\n\n*_Nos pondremos en contacto contigo a la brevedad posible️_*', m)
 }
 handler.help = ['bug <reporte>', 'report <reporte>']
