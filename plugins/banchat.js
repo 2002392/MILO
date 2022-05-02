066ea473856872d7387315ba90aeb23426f54594
danerybaez27 <104737615+danerybaez27@users.noreply.github.com>
2022-05-02 13:52:27 -0400

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn, participants }) => {
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     if (!(m.chat in global.DATABASE._data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
     let chat = global.DATABASE._data.chats[m.chat]
-    if (chat.isBanned) return m.reply('*Este chat ya estaba baneado previamente*\n*Si desea desbanearlo use /unbanchat*')
+    if (chat.isBanned) return m.reply('*Deja tu loquera que el Bot Danery hanma ya estaba apagado*\n*Si desea desbanearlo use /unbanchat*')
     chat.isBanned = true
-    m.reply('*Este chat ha sido baneado a desicion de un administrador del grupo*')
+    m.reply('*El bot de Danery hanma se apago correctamente a desicion de un administrador del grupo asi que ya no les voy hacer caso putos*')
   // } else m.reply('Aquí hay un número de un host...')
 }
 handler.help = ['banchat']
