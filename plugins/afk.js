9c7f94ee211f3bc810fb05d5ac90f69ac5aef950
danerybaez27 <104737615+danerybaez27@users.noreply.github.com>
2022-05-02 13:46:50 -0400

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -2,7 +2,7 @@ let handler = async (m, { text }) => {
   let user = global.DATABASE.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  m.reply(`*[ ⚠️ ] El usuario ${conn.getName(m.sender)} estará inactivo (AFK) no lo etiqueten*\n\n*🔰 Motivo de la inactividad${text ? ': ' + text : ''}*
+  m.reply(`*[ ⚠️ ] atención hijos de XD soy Danery baez el mejor bot anuncio  que el usuario ${conn.getName(m.sender)} estará inactivo (AFK) no lo etiqueten*\n\n*🔰 Motivo de la inactividad${text ? ': ' + text : ''}*
 `)
 }
 handler.help = ['afk [alasan]']
