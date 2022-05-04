dcd5601d39c5da8cc80d06956b87e96d5511d2f6
danerybaez27 <104737615+danerybaez27@users.noreply.github.com>
2022-05-04 17:49:06 -0400

    Add files via upload

--- plugins/so-hentai.mp3.js
+++ plugins/so-hentai.mp3.js
@@ -2,12 +2,14 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/Yamete-kudasai.mp3'
-conn.sendFile(m.chat, vn, 'Yamete-kudasai.mp3', null, m, true, {
+let vn = './media/hentai.mp3'
+conn.sendFile(m.chat, vn, 'hentai.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true
 })
 }
-handler.customPrefix = /Yamete|yamete|Yamete kudasai|yamete kudasai/ 
+handler.customPrefix = /audio hentai|Audio hentai|audiohentai|Audiohentai/i 
 handler.command = new RegExp
+
 module.exports = handler
+
--- plugins/yoshi-cancion_mp3.js
+++ plugins/yoshi-cancion_mp3.js
@@ -1,16 +1,13 @@
 let util = require('util')
-let path = require('path')
+let path = require('path') 
 
 let handler = async (m, { conn }) => {
-let vn = './media/No soy real.mp3'
-conn.sendFile(m.chat, vn, 'No soy real.mp3', null, m, true, {
+let vn = './media/yoshi-cancion.mp3'
+conn.sendFile(m.chat, vn, 'yoshi-cancion.mp3', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /bot te amo|botteamo|boteamo|te quiero bot/i 
+handler.customPrefix = /YOSHI|Yoshi|YoShi|yoshi|/i
 handler.command = new RegExp
-
-handler.fail = null
-handler.exp = 100
 module.exports = handler
