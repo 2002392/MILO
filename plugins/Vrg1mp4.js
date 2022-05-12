a37b5ca9382742a67dc7977c13d2f57ace643c42
danerybaez27 <104737615+danerybaez27@users.noreply.github.com>
2022-05-12 12:08:32 -0400

    Add files via upload

---
+++ plugins/Vrg1mp4.js
@@ -0,0 +1,39 @@
+let PhoneNumber = require('awesome-phonenumber')
+let levelling = require('../lib/levelling')
+
+let handler = async (m, { conn, usedPrefix }) => {
+
+  let pp = './1.mp4'
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  try {
+//    pp = await conn.getProfilePicture(who)
+  } catch (e) {
+
+  } finally {
+    let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
+    let { name, limit, exp, banned, lastclaim, registered, regTime, age, level } = global.DATABASE.data.users[m.sender]
+    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+    let username = conn.getName(who)
+    let str = `
+╭══〘 🐈⚡️🐈⚡️🐈⚡️🐈 〙══╮    
+⎪ *_ミ💖 ${username} 💖彡_*
+
+    let mentionedJid = [who]
+    conn.sendFile(m.chat, pp, 'lp.jpg', str, m, false, { contextInfo: { mentionedJid }})
+  }
+}
+handler.help = ['menu2']
+handler.tags = ['General']
+handler.command = /^(vete a la verga||jodete||vtalvrg|)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
