0b860982fd919e0075905452bd32d93a60dcce74
danerybaez27 <104737615+danerybaez27@users.noreply.github.com>
2022-05-12 00:33:17 -0400

    Update _Bot.js

--- plugins/_Bot.js
+++ plugins/_Bot.js
@@ -1,6 +1,6 @@
 let fs = require("fs")
 let handler = async(m, { conn, args, text, usedPrefix: _p, usedPrefix, command }) => {
-let vn = './media/entrada.mp3'
+let vn = './videos/entrada.mp4'
 const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
 message: { 
 orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'WhatsApp Bot Oficial', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'    
@@ -10,7 +10,7 @@ message: {
 "audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"   
 }}}  
 conn.sendButton(m.chat, `*Hola soy DaneryBot, como te puedo ayudar?*`, '©Danery baez  - Bot', '𝙼𝙴𝙽𝚄 𝙳𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂', `${usedPrefix}menu`, 'conversation', { sendEphemeral: true, quoted: estilo })
-await conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true, quoted: estiloaudio })
+await conn.sendFile(m.chat, vn, 'video', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true, quoted: estiloaudio })
 }
 handler.command = /^(epico)$/epico
 module.exports = handler
