93d11867dd7f6f81efe33ed5d01b2464f97f74ce
danerybaez27 <104737615+danerybaez27@users.noreply.github.com>
2022-05-14 22:23:23 -0400

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -1,9 +1,11 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 *Hola 👋🏻, unete al grupo oficial para pasar un rato agradable usando el Bot*
-Grupo oficial https://chat.whatsapp.com/EVrT2S9PC4bKzxRgcbKdll
+Grupo oficial https://chat.whatsapp.com/C8oB5zed0rY6xqLqfpqps4
 *_➤ creador Danery contactalo:_*
 *_1.-_* *wa.me/18292077732*
+*_➤ Bot oficial:_*
+*_1.-_* *wa.me/573106442515*
 `.trim() 
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥DaneryBot🔥*', 'status@broadcast')
