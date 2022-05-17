8c92d6c438570b80cf2e51d172d315c005df430c
danerybaez27 <104737615+danerybaez27@users.noreply.github.com>
2022-05-17 13:54:07 -0400

    Update Bts.js

--- plugins/Bts.js
+++ plugins/Bts.js
@@ -2,7 +2,7 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/Calla fan de BTS.mp3'
+let vn = './media/Calla Fan de BTS.mp3'
 conn.sendFile(m.chat, vn, 'Feliz cumple.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
