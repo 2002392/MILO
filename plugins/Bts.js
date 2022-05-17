2b23b18bf0142a4358a14e0b8f3d7fceede37aac
danerybaez27 <104737615+danerybaez27@users.noreply.github.com>
2022-05-17 13:51:00 -0400

    Create Bts.js

---
+++ plugins/Bts.js
@@ -0,0 +1,18 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/Calla fan de BTS.mp3'
+conn.sendFile(m.chat, vn, 'Feliz cumple.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /bts|Bts|Fan de bts/i
+handler.command = new RegExp
+
+
+
+handler.fail = null
+handler.exp = 100
+module.exports = handler
