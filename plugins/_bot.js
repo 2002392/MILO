93e1d1ba04781b68caf332f8584533c493cde877
danerybaez27 <104737615+danerybaez27@users.noreply.github.com>
2022-05-17 20:21:31 -0400

    Update _bot.js

--- plugins/_bot.js
+++ plugins/_bot.js
@@ -1,6 +1,6 @@
 let fs = require("fs")
 let handler = async(m, { conn, args, text, usedPrefix: _p, usedPrefix, command }) => {
-let vn = './media/bot.mp3'
+let vn = './media/Gemi2Remix.mp3'
 const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
 message: { 
 orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'WhatsApp Bot Oficial', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'    
@@ -10,7 +10,7 @@ message: {
 "audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"   
 }}}  
 conn.sendButton(m.chat, `*Hola, como te puedo ayudar?*`, '©Danery hanma- Bot', '𝙼𝙴𝙽𝚄 𝙳𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂', `${usedPrefix}menu`, 'conversation', { sendEphemeral: true, quoted: estilo })
-await conn.sendFile(m.chat, vn, './media/bot.mp3', null, m, true, {
+await conn.sendFile(m.chat, vn, './media/Gemi2Remix.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true })
 }
