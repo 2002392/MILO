99981cbb18a2a4af26cd8d92f1c9ad89f8adf32c
danerybaez27 <104737615+danerybaez27@users.noreply.github.com>
2022-05-29 11:59:45 -0400

    Create Maricon.js

---
+++ plugins/Maricon.js
@@ -0,0 +1,30 @@
+//NO MODIFIQUES EL NÚMERO DEL CREADOR NI EL NOMBRE.. SOLO AGREGA LA INFORMACIÓN QUE TU QUIERAS O EDITALO A TU MANERA PERO DEJANDO LOS CREDITOS <3 
+//PUEDES AGREGAR OTRA FILAS DE PAYPAL, GRUPOS, PERO DEJA ALGUNOS CREDITOS, YA QUE ES LA UNICA MANERA DE INGRESOS DEL BOT
+//SI VAS A MODIFICAR TODO Y HACER PASAR COMO SI FUERA TU BOT (CREADO POR TI) SOLO TE PIDO QUE SI ESTA EN TUS POSIBILIDADES DONES UN POCO
+//LLEVO AL REDEDOR DE 1 AÑO Y MEDIO EN LA ELABORACION DE ESTE BOT, SI TU LO EDITAS EN UNA SEMANA NO ERES TU EL CREADOR, SI LO EDITAS TOTALMENTE A TU MANERA SERIA UTULIZADO MI BOT COMO BASE Y AHI SI LO SERIAS  
+
+let fs = require('fs')
+let fetch = require('node-fetch')
+let moment = require('moment-timezone')
+let path = require('path')
+let util = require('util')
+let handler = async (m, { conn, usedPrefix }) => {
+let pp = './maricon.jpg'
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let username = conn.getName(who)
+//let vn = './media/mariana.mp3'
+let menu =`
+╭══〘 ✯✯✯✯✯✯✯✯ 〙═╮
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈
+${username}!!* maricon
+┗━━━━━━━━━━━━━┛`.trim()
+let mentionedJid = [who]
+conn.send3ButtonImg(m.chat, pp, menu, '© Danery,Camilo bot - Bot', 'Que gay', `#jaja`, 'Que coma vrf', `#wtf`, 'violemoslo', `#xd`, m, false, { contextInfo: { mentionedJid }})   
+//await await await await await await conn.sendFile(m.chat, vn, 'mariana.mp3', null, m, false, {
+//type: 'audioMessage', 
+//ptt: true 
+//})
+}
+handler.command = /^(🥺|🥺|🥺|🥺|🥺|🥺|🥺|🥺|🥺|🥺|🥺|🥺|🥺)$/i
+handler.fail = null
+module.exports = handler
