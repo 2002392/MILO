3dce2e60feeaa50bf0310587cb05f72ae4702dee
2002392 <107109208+2002392@users.noreply.github.com>
2022-06-09 23:46:04 -0500

    Update and rename plugins/estado.js to complementos/estado.js

--- complementos/estado.js
+++ complementos/estado.js
@@ -1,12 +1,11 @@
-let fetch = require('node-fetch')
 let handler = async (m, { conn, command, text, usedPrefix }) => {
   await conn.send3Button(m.chat, `
 *ミ💖 Hola estimado usuario🍷🧐 Soy el puto bot   💖彡*
 
 *ミ🤖 Estado del Bot 🤖彡*
-*=> BotDanery activo ✔️*
+*=> BotMILO activo ✔️*
 *=> Bot uso público ✔️*
-`.trim(), '©Danery Bot', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '🔶️ MENÚ SIMPLE 🔶️', `${usedPrefix}menusimple`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menuaudios`)
+`.trim(), '©MILO Bot', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '🔶️ MENÚ SIMPLE 🔶️', `${usedPrefix}menusimple`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menuaudios`)
 }
 handler.command = /^(estado|status|estate|state|stado|stats)$/i
 
