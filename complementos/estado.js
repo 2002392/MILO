da31a1a7e0aab293f067a5ff56d2341a3ac1267c
2002392 <107109208+2002392@users.noreply.github.com>
2022-06-10 01:15:59 -0500

    Update estado.js

--- complementos/estado.js
+++ complementos/estado.js
@@ -1,19 +1,18 @@
-let    fetch    =    require ( 'node-fetch' )
-let  handler  =  async  ( m ,  { conn , command , text , usedPrefix } )  =>  {
-  esperar  contacto . enviar3Button ( m . chat ,  `
-*ミ Hola estimado usuario🍷🧐 Soy el puto bot ;v 彡*
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  await conn.send3Button(m.chat, `
+*ミ💖 Hola estimado usuario🍷🧐 Soy el puto bot   💖彡*
+
 *ミ🤖 Estado del Bot 🤖彡*
-
-*=> BotMILo activo ✔️*
+*=> BotMILO activo ✔️*
 *=> Bot uso público ✔️*
-` . trim ( ) ,  '©MILO Bot' ,  '🔷️ MENÚ 🔷' ,  ` ${ usedPrefix } menu` ,  '🔶️ MENÚ SIMPLE 🔶️' ,  ` ${ usedPrefix } menusimple` ,  '💠️ MENÚ AUDIOS 💠️' ,  ` ${ usedPrefix } menúaudios` )
+`.trim(), '©MILO Bot', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '🔶️ MENÚ SIMPLE 🔶️', `${usedPrefix}menusimple`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menuaudios`)
 }
-manejador _ comando  =  / ^ ( estado | estado | propiedad | estado | estado | estadísticas ) $ / i
+handler.command = /^(estado|status|estate|state|stado|stats)$/i
 
-manejador _ exp  =  0
+handler.exp = 0
 
-modulo _ exportaciones = controlador
-
+module.exports = handler
 
 
 
